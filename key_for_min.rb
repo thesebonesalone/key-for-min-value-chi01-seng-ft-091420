79d6c74b157dce36803ec7b5894bf305b054f9a5
Willem A Duiker <duikhead2@gmail.com>
2020-08-22 16:13:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min = name_hash[0]
+  min = 10000
   lowkey = nil
   name_hash.each do |key, value|
     puts key
